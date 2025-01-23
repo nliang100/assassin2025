ab104c56f649a3bbfe5b497053511bdf6025f609
nliang100 <100380931+nliang100@users.noreply.github.com>
2025-01-22 22:46:27 -0500

    Fix Heroku configuration

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ def send_email(killer_name, target_name, recipient_email):
         smtp.send_message(em)
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False)
 
